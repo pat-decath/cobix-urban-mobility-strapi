a3a354977f1645da63a2441c7b8f067e0cc5c09a
Rémi Salmi <remi.salmi@frog.co>
2024-01-22 09:41:13 +0100

    ✨ Add tutorials and tags collections

---
+++ src/api/tutorial-tag/controllers/tutorial-tag.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * tutorial-tag controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::tutorial-tag.tutorial-tag');
